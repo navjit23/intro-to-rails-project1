f6bad19757b5b0b08b8359eb2f3ac338b367e7bb
navjit23 <66906714+navjit23@users.noreply.github.com>
2023-07-12 10:56:28 -0500

    v2
    added league dat tried :-1:

--- testtesttest.rb
+++ testtesttest.rb
@@ -1,16 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 
-require 'json'
-require 'rest-client'
-require 'open-url'
+
 require 'net/http'
-
+require 'uri'
 
 url = URI("https://football98.p.rapidapi.com/competitions")
 url_for_tables = URI("https://football98.p.rapidapi.com/premierleague/table")
@@ -26,10 +17,9 @@ request = Net::HTTP::Get.new(url)
 request["X-RapidAPI-Key"] = 'd78181db48msh50182f7ef3451f8p1ff6efjsnf9e2245d8348'
 request["X-RapidAPI-Host"] = 'football98.p.rapidapi.com'
 
-
 response = http.request(request)
 x = response.read_body
 league_names = x.split(",")
 league_names.each do |league|
-  League.create(name :league)
+  puts league
 end
