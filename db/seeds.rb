f55c903eb8566586904adcdcb0157a4bedc0a90c
navjit23 <66906714+navjit23@users.noreply.github.com>
2023-07-12 12:59:29 -0500

    added leagues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-require 'json'
-require 'rest-client'
-require 'open-url'
+require 'uri'
 require 'net/http'
+require 'json'
 
+LEAGUE_IDS = [39, 40]
+TEAM_IDS = [11,2,4]
 
-url = URI("https://football98.p.rapidapi.com/competitions")
-url_for_tables = URI("https://football98.p.rapidapi.com/premierleague/table")
-url_for_fixtures = URI("https://football98.p.rapidapi.com/premierleague/fixtures")
-
-
-
+url = URI("https://api-football-v1.p.rapidapi.com/v3/leagues?id=39")
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
 
 request = Net::HTTP::Get.new(url)
 request["X-RapidAPI-Key"] = 'd78181db48msh50182f7ef3451f8p1ff6efjsnf9e2245d8348'
-request["X-RapidAPI-Host"] = 'football98.p.rapidapi.com'
-
+request["X-RapidAPI-Host"] = 'api-football-v1.p.rapidapi.com'
 
 response = http.request(request)
-x = response.read_body
-league_names = x.split(",")
-league_names.each do |league|
-  League.create(name :league)
-end
+data = JSON.parse(response.read_body)
+
+league_name = data["response"][0]["league"]["name"]
+League.create(league_name: league_name)
+
+
--- testtesttest.rb
+++ testtesttest.rb
@@ -1,25 +1,71 @@
-
-
-require 'net/http'
 require 'uri'
+require 'net/http'
+require 'json'
 
-url = URI("https://football98.p.rapidapi.com/competitions")
-url_for_tables = URI("https://football98.p.rapidapi.com/premierleague/table")
-url_for_fixtures = URI("https://football98.p.rapidapi.com/premierleague/fixtures")
-
-
-
+url = URI("https://api-football-v1.p.rapidapi.com/v3/players?team=33&season=2022")
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
 
 request = Net::HTTP::Get.new(url)
 request["X-RapidAPI-Key"] = 'd78181db48msh50182f7ef3451f8p1ff6efjsnf9e2245d8348'
-request["X-RapidAPI-Host"] = 'football98.p.rapidapi.com'
+request["X-RapidAPI-Host"] = 'api-football-v1.p.rapidapi.com'
 
 response = http.request(request)
-x = response.read_body
-league_names = x.split(",")
-league_names.each do |league|
-  puts league
+data = JSON.parse(response.read_body)
+
+#loop the 0 thing
+team_names = data["response"][1]["player"]
+puts team_names
+# have 10 leagues
+# url = URI("https://api-football-v1.p.rapidapi.com/v3/leagues?id=39")
+# each league have 20 team
+#url=URI("https://api-football-v1.p.rapidapi.com/v3/teams?league=39&season=2022")
+# each league have 20 player
+#url = URI("https://api-football-v1.p.rapidapi.com/v3/players?team=33&season=2022")
+
+
+
+
+# FOR LEAGUE NAMES
+=begin
+
+rescue => exception
+
 end
+url = URI("https://api-football-v1.p.rapidapi.com/v3/leagues?id=39")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+request["X-RapidAPI-Key"] = 'd78181db48msh50182f7ef3451f8p1ff6efjsnf9e2245d8348'
+request["X-RapidAPI-Host"] = 'api-football-v1.p.rapidapi.com'
+
+response = http.request(request)
+data = JSON.parse(response.read_body)
+
+league_names = data["response"][0]["league"]["name"]
+puts league_names
+=end
+
+
+# FOR TEAM NAMES
+#remember to take the team id for players
+=begin
+url = URI("https://api-football-v1.p.rapidapi.com/v3/teams?league=39&season=2022")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+request["X-RapidAPI-Key"] = 'd78181db48msh50182f7ef3451f8p1ff6efjsnf9e2245d8348'
+request["X-RapidAPI-Host"] = 'api-football-v1.p.rapidapi.com'
+
+response = http.request(request)
+data = JSON.parse(response.read_body)
+
+#loop the 0 thing
+team_names = data["response"][0]["team"]["name"]
+puts team_names
+=end
