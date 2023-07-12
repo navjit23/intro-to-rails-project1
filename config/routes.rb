eebb56c1cb8ac66c040e61e2b20199b8d3adfc24
navjit23 <66906714+navjit23@users.noreply.github.com>
2023-07-12 16:11:18 -0500

    added bootsrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+
   resources :players
   resources :fixtures
   resources :tables
   resources :leagues
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  get 'home/index'
+  get 'home/about'
 end
